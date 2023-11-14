cb7205baa894c46643cf78727b800e82dbe78fd4
johnpmarshall2000 <johnpmarshall2000@gmail.com>
2023-11-13 22:47:12 -0500

    Changed Sortable and Updated Sorts

--- sorts.py
+++ sorts.py
@@ -1,65 +1,76 @@
 import random
 import copy
 
+DEBUG = False
+
 class Sortable:
     """Sortable - Manages the instructions produced by a Sort.
     """
-    def __init__(self, sortable, colors = {"Read":(255, 0, 0), "Write":(64, 64, 255), "Set":(255, 255, 255), "Swap":(64, 64, 255), "Compare":(255, 255, 0)}):
-        self.sortable = sortable
+    def __init__(self, sortable: list, colors: dict = {"Read":(255, 0, 0), "Write":(64, 64, 255), "Set":(255, 255, 255), "Swap":(64, 64, 255), "Compare":(255, 255, 0)}):
+        self.data = sortable
+        self.is_sorted: bool = False
+
         self.colors = colors
-        self.is_integer = all([type(i) is int for i in sortable])
+        self.colored: list = []
+
+        self.steps: int = 0
+        self.modifying_steps: int = 0
     
-    def run_steps(self, sort, debug = False):
+    def read(self, index):
+        self.colored = [(index, self.colors["Read"])]
+        self.steps += 1
 
-        sortable = copy.deepcopy(self.sortable)
+        return self.data
 
-        for instruction in sort.get_steps(self.sortable):
-            args = instruction.split(" ")
-            count = instruction.count(" ")
+    def reads(self, start_index, end_index): # Includes both end indices
+        for index in range(start_index, end_index + 1):self.read(index)
 
-            if args[0] == "Set":
-                new_sortable = "".join(args[1:])[1:-1].split(",")
-                new_sortable = [self.str_to_num(i) for i in new_sortable]
-            elif count == 1:a = int(args[1][1:])
-            elif count == 2:
-                a = int(args[1][1:])
-                b = args[2]
-                if b[0] == "$":b = int(b[1:])
-                else:b = self.str_to_num(b)
+        return self.data
+    
+    def write(self, index, value):
+        self.colored = [(index, self.colors["Write"])]
+        self.steps += 1
+        self.modifying_steps += 1
 
-            # yield (instruction_type, affected_indices, color, sortable, is_sortable_changed)
-
-            if args[0] == "Read":
-                if debug:print(f"Sortable[{a}] ({sortable[a]})")
-                yield ("Read", set((a,)), self.colors["Read"], sortable, False)
-
-            elif args[0] == "Write":
-                sortable[a] = b
-                if debug:print(f"Sortable[{a}] <- {b}")
-                yield ("Write", set((a,)), self.colors["Write"], sortable, True)
-
-            elif args[0] == "Swap":
-                sortable[a], sortable[b] = sortable[b], sortable[a]
-                if debug:print(f"Sortable[{b}] ({sortable[b]}) <-> Sortable[{a}] ({sortable[a]})")
-                yield ("Swap", set((a, b)), self.colors["Swap"], sortable, True)
-
-            elif args[0] == "Set":
-                sortable = new_sortable.copy()
-                if debug:print(f"Sortable <- {new_sortable}")
-                yield ("Set", None, self.colors["Set"], sortable, True)
-
-            elif args[0] == "Compare":
-                if debug:print(f"Sortable[{a}] ({sortable[a]}) vs Sortable[{b}] ({sortable[b]})")
-                yield ("Compare", set((a, b)), self.colors["Compare"], sortable, False)
-
-            elif args[0] == "Done":
-                if debug:print(f"Done | {sortable}")
-                yield ("Done", None, None, sortable, False)
+        self.data[index] = value
+        return self.data
+    
+    def writes(self, start_index, values):
+        for offset in range(len(values)):
+            self.write(start_index + offset, values[offset])
         
-        print(sortable)
+        return self.data
     
-    def str_to_num(self, string):
-        return int(string) if self.is_integer else float(string)
+    def swap(self, index_a, index_b):
+        self.colored = [(index_a, self.colors["Swap"]), (index_b, self.colors["Swap"])]
+        self.steps += 1
+        self.modifying_steps += 1
+
+        self.data[index_a], self.data[index_b] = self.data[index_b], self.data[index_a]
+        return self.data
+    
+    def set(self, new_sortable):
+        self.colored = [("Set", self.colors["Set"])]
+        self.steps += 1
+        self.modifying_steps += 1
+
+        self.data = new_sortable
+        return self.data
+    
+    def compare(self, index_a, index_b):
+        self.colored = [(index_a, self.colors["Compare"]), (index_b, self.colors["Compare"])]
+        self.steps += 1
+
+        return self.data
+    
+    def sorted(self):
+        self.is_sorted = True
+    
+    def not_sorted(self):
+        self.is_sorted = False
+
+    def draw(self):
+        pass
 
 class Sort:
     def __init__(self):
@@ -76,34 +87,29 @@ class Sort:
 # Compare $a $b - Compare the values at index a and index b
 # Done - Sorting is done
 
-DEBUG = False
-
 class BubbleSort(Sort):
     """Bubble Sort - Iterates over all elements, swapping them if the first element is larger than the second.
     """
     def __init__(self):
         super().__init__()
     
-    def get_steps(self, sortable):
+    def run(self, sortable: Sortable):
 
         while True:
-            is_sorted = True
+            sortable.sorted()
 
-            for index in range(len(sortable) - 1):
-                yield f"Read ${index}"
-                yield f"Read ${index + 1}"
-                yield f"Compare ${index} ${index + 1}"
+            for index in range(len(sortable.data) - 1):
+                yield sortable.read(index)
+                yield sortable.read(index + 1)
+                yield sortable.compare(index, index + 1)
 
-                if sortable[index] > sortable[index + 1]:
-                    yield f"Swap ${index} ${index + 1}"
-                    is_sorted = False
-                    sortable[index], sortable[index + 1] = sortable[index + 1], sortable[index]
+                if sortable.data[index] > sortable.data[index + 1]:
+                    yield sortable.swap(index, index + 1)
+                    sortable.not_sorted()
 
-            if is_sorted:
-                yield "Done"
-                break
+            if sortable.is_sorted:break
         
-        if DEBUG:print(sortable)
+        if DEBUG:print(sortable.data)
 
 class InsertionSort(Sort):
     """Insertion Sort - Sorts by moving individual element to the correct position in the already sorted portion, starting with just the first element.
@@ -111,36 +117,35 @@ class InsertionSort(Sort):
     def __init__(self):
         super().__init__()
     
-    def get_steps(self, sortable):
+    def run(self, sortable: Sortable):
 
         index = 0
-        while index < len(sortable) - 1:
+        while index < len(sortable.data) - 1:
             # Increment the index of the current element
             index += 1
 
-            yield f"Read ${index}"
-            yield f"Read ${index + 1}"
-            yield f"Compare ${index} ${index + 1}"
+            yield sortable.read(index)
+            yield sortable.read(index + 1)
+            yield sortable.compare(index, index + 1)
 
             # The order of the element is already correct
-            if sortable[index - 1] <= sortable[index]:continue
+            if sortable.data[index - 1] <= sortable.data[index]:continue
 
             # Continue pushing the element until it reaches the correct spot
             for offset in range(index):
                 b = index - offset
                 a = b - 1
 
-                yield f"Read ${a}"
-                yield f"Read ${b}"
-                yield f"Compare ${a} ${b}"
+                yield sortable.read(a)
+                yield sortable.read(b)
+                yield sortable.compare(a, b)
 
                 # The element has reached the correct position
-                if sortable[a] <= sortable[b]:break
+                if sortable.data[a] <= sortable.data[b]:break
 
-                yield f"Swap ${a} ${b}"
-                sortable[a], sortable[b] = sortable[b], sortable[a]
-        yield "Done"
-        if DEBUG:print(sortable)
+                yield sortable.swap(a, b)
+
+        if DEBUG:print(sortable.data)
 
 class SelectionSort(Sort):
     """Selection Sort - Sorts by moving the smallest element in the unsorted portion to the end of the sorted portion.
@@ -148,27 +153,25 @@ class SelectionSort(Sort):
     def __init__(self):
         super().__init__()
     
-    def get_steps(self, sortable):
+    def run(self, sortable: Sortable):
 
-        for index in range(len(sortable) - 1):
-            yield f"Read ${index}"
-            minimum = sortable[index], index
+        for index in range(len(sortable.data) - 1):
+            yield sortable.read(index)
+            minimum = sortable.data[index], index
 
             # Find the minimum element in the unsorted section of the sortable
-            for unsorted_index in range(index + 1, len(sortable)):
-                yield f"Read ${unsorted_index}"
-                yield f"Compare ${minimum[1]} ${unsorted_index}"
+            for unsorted_index in range(index + 1, len(sortable.data)):
+                yield sortable.read(unsorted_index)
+                yield sortable.compare(minimum[1], unsorted_index)
                 
-                current = sortable[unsorted_index]
+                current = sortable.data[unsorted_index]
                 if minimum[0] > current:minimum = current, unsorted_index
 
             # Place the minimum unsorted element at the end of the sorted section
             if index == minimum[1]:continue
-            yield f"Swap ${index} ${minimum[1]}"
-            sortable[index], sortable[minimum[1]] = sortable[minimum[1]], sortable[index]
+            yield sortable.swap(index, minimum[1])
         
-        yield "Done"
-        if DEBUG:print(sortable)
+        if DEBUG:print(sortable.data)
 
 class MergeSort(Sort):
     """Merge Sort - Sorts by merging adjacent sorted portions.
@@ -177,69 +180,51 @@ class MergeSort(Sort):
     def __init__(self):
         super().__init__()
     
-    def get_steps(self, sortable):
-        instructions, sortable = self.merge(sortable, 0)
-        for instruction in instructions:
-            yield instruction
-        yield "Done"
-        #print(sortable)
-
-    def merge(self, sortable, offset):
-        if len(sortable) == 1:
-            #print(sortable)
-            return [[f"Read ${offset}"], sortable]
+    def run(self, sortable: Sortable, start_index = 0, end_index = None):
+        if end_index is None:end_index: int = len(sortable.data)
+        if start_index + 1 == end_index:pass
         else:
-            split_index = self.get_split(len(sortable))
+            length = end_index - start_index
+            split_index = self.get_split(length) + start_index
 
-            instructions_a, a = self.merge(sortable[:split_index], 0)
-            instructions_b, b = self.merge(sortable[split_index:], split_index)
+            small_iter = self.run(sortable, start_index, split_index)
+            for call in small_iter:yield call
+            
+            big_iter = self.run(sortable, split_index, end_index)
+            for call in big_iter:yield call
 
-            instructions = instructions_a + instructions_b
-
-            offset_a, offset_b = offset, len(a) + offset
-            index_a, index_b = 0, 0
             auxiliary = []
+            small = sortable.data[start_index:split_index]
+            big = sortable.data[split_index:end_index]
 
-            for i in range(len(a) + len(b)):
+            small_index, big_index = 0, 0
+            for _ in range(length):
 
-                # a or b have been exhausted
+                if small_index == len(small):
+                    yield sortable.reads(split_index + big_index, split_index + len(big) - 1)
+                    auxiliary += big[big_index:]
+                    break
 
-                if index_a == len(a):
-                    for j in range(index_b, len(b)):
-                        instructions.append(f"Write ${j + offset_b} {b[j]}")
-                        auxiliary.append(b[j])
-                    #print(auxiliary)
-                    return [instructions, auxiliary]
+                elif big_index == len(big):
+                    yield sortable.reads(start_index + small_index, start_index + len(small) - 1)
+                    auxiliary += small[small_index:]
+                    break
 
-                elif index_b == len(b):
-                    for j in range(index_a, len(a)):
-                        instructions.append(f"Write ${j + offset_a} {a[j]}")
-                        auxiliary.append(a[j])
-                    #print(auxiliary)
-                    return [instructions, auxiliary]
-                
-                # Pick the smaller of the smallest value in a and b
-                
-                instructions += [
-                    f"Read ${index_a + offset_a}",
-                    f"Read ${index_b + offset_b}",
-                    f"Compare ${index_a + offset_a} ${index_b + offset_b}"
-                ]
-
-                if a[index_a] <= b[index_b]:
-                    instructions.append(f"Write ${i + offset} {a[index_a]}")
-                    auxiliary.append(a[index_a])
-                    index_a += 1
+                elif small[small_index] <= big[big_index]:
+                    yield sortable.read(start_index + small_index)
+                    auxiliary.append(small[small_index])
+                    small_index += 1
 
                 else:
-                    instructions.append(f"Write ${i + offset} {b[index_b]}")
-                    auxiliary.append(b[index_b])
-                    index_b += 1
+                    yield sortable.read(split_index + big_index)
+                    auxiliary.append(big[big_index])
+                    big_index += 1
+            
+            yield sortable.writes(start_index, auxiliary)
 
     def get_split(self, sortable_length):
         index = 1
-        while index < sortable_length:
-            index <<= 1
+        while index < sortable_length:index <<= 1
         return index >> 1
 
 class Quicksort(Sort):
@@ -248,7 +233,7 @@ class Quicksort(Sort):
     def __init__(self):
         super().__init__()
     
-    def get_steps(self, sortable):
+    def run(self, sortable: Sortable):
         pass
 
     def get_pivot(self, sortable_length):
@@ -260,48 +245,48 @@ class Bogosort(Sort):
     def __init__(self):
         super().__init__()
     
-    def get_steps(self, sortable):
+    def run(self, sortable: Sortable):
 
         # Loops until sorted
         while True:
 
             # Checking each adjacent pair of values is in the correct order
-            sort = True
-            for i in range(len(sortable) - 1):
-                yield f"Read ${i}"
-                yield f"Read ${i + 1}"
-                yield f"Compare ${i} ${i + 1}"
+            sortable.sorted()
+            for i in range(len(sortable.data) - 1):
+                yield sortable.read(i)
+                yield sortable.read(i + 1)
+                yield sortable.compare(i, i + 1)
 
                 # If a pair of values is not in the correct order, sortable is not sorted
-                if sortable[i] > sortable[i + 1]:
-                    sort = False
+                if sortable.data[i] > sortable.data[i + 1]:
+                    sortable.not_sorted()
                     break
 
             # Sortable is successfully sorted    
-            if sort:
-                yield "Done"
-                break
+            if sortable.is_sorted:break
 
             # If it isn't sorted, randomize it
-            random.shuffle(sortable)
-            yield f"Set {sortable}"
+            temp = sortable.data.copy()
+            random.shuffle(temp)
+            yield sortable.set(temp)
+            
         if DEBUG:print(sortable)
 
 if __name__ == "__main__":
     # Sortable to be sorted
-    s = list(range(512))
+    s = list(range(16))
     random.shuffle(s)
     sortable = Sortable(s)
-    print(sortable.sortable)
+    print(sortable.data)
 
     # Sort object
-    #print(BubbleSort.__doc__)
     sort = MergeSort()
 
     # Sorting
-    sorting = sortable.run_steps(sort)
-    steps = []
-    for i in sorting:
-        steps.append(i)
-    print("Steps:", len(steps))
-    print("Modifying Steps:", len([i for i in steps if i[-1]]))
+    print("Starting sort of", len(s), "elements")
+    step_count = 0
+    for i in sort.run(sortable):
+        step_count += 1
+        #print(i, sortable.colored)
+    print(sortable.data)
+    print("Steps:", step_count)
